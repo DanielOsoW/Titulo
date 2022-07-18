7ad8d517e0f79918850d8540de05a7dfa9c9db3f
Daniel Osorio <daniel.osorio.w@usach.cl>
2022-07-18 19:41:13 -0400

    Connection

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import * as VuePrismEditor from "vue-prism-editor"
 Vue.config.productionTip = false
 
 const axiosInstance = axios.create({ 
-  baseURL: 'http://164.92.79.203/:8081/'
+  baseURL: 'http://159.203.173.102:8081/'
 })
 
 //Para acceder a axios desde this.$http.
